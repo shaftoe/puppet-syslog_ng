29cc34c645bdacf4c8d78d008c459579cc0b2db8
Alexander Fortin <alexander.fortin@gmail.com>
2013-09-10 22:57:37 +0200

    Register to loggly

--- manifests/config.pp
+++ manifests/config.pp
@@ -1,7 +1,17 @@
 #
-# Config files
+# Config files and register device to loggly
 #
-class syslog_ng::config () inherits syslog_ng {
+class syslog_ng::config(
+  $subdomain = false,
+  $input = false,
+  $user = false,
+  $pass = false
+  ) inherits syslog_ng {
+
+  if ! $subdomain { fail 'You need to provide $subdomain' }
+  if ! $input     { fail 'You need to provide $input' }
+  if ! $user      { fail 'You need to provide $user' }
+  if ! $pass      { fail 'You need to provide $pass' }
 
   require 'syslog_ng::package'
 
@@ -10,6 +20,14 @@ class syslog_ng::config () inherits syslog_ng {
     owner   => 'root',
     mode    => '0440',
     content => template('syslog_ng/syslog-ng.conf.erb')
+  }
+
+  $curl = "/usr/bin/curl -X POST -u ${user}:${pass}"
+  $url = "http://${subdomain}.loggly.com/api/inputs/${input}/adddevice"
+  $placeholder = "/etc/syslog-ng/${::hostname}.enabled"
+  exec { 'register_to_loggly':
+    command => "${curl} ${url} && /usr/bin/touch ${placeholder}",
+    creates => $placeholder,
   }
 
 }
